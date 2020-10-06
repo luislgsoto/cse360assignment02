4402014db1b5eeb84f02577226d0e75fece9d2d4
Luis Gonzalez <luislgsoto@gmail.com>
2020-10-06 14:49:31 -0700

    Create the initial version

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,40 @@
+
+package cse360assignment02;
+
+/**
+ *
+ * @author Luis Gonzalez
+ * class that adds and subtracts
+ */
+public class AddingMachine 
+{
+  private int total;
+  
+  public AddingMachine () 
+  {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () 
+  {
+    return 0;
+  }
+  
+  public void add (int value) 
+  {
+  }
+
+  public void subtract (int value) 
+  {
+  }
+
+  public String toString () 
+  {
+    return "";
+  }
+
+  public void clear() 
+  {
+  }
+    
+}
